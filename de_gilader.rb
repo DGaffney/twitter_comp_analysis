e80351311e5713c76ed2a1121ee071501c473403
Devin Gaffney <itsme@devingaffney.com>
2011-01-30 12:33:02 -0800

    blah butts

--- de_gilader.rb
+++ de_gilader.rb
@@ -7,7 +7,11 @@ DataMapper.setup(:default, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepag
 DataMapper.setup(:tunisia, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_1')
 DataMapper.setup(:iran, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch')
 DataMapper.setup(:egypt, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_2')
-
+DataMapper.repository(:tunisia){User.count}
+tt = DataMapper.repository(:tunisia){Tweet.count}
+ttd = DataMapper.repository(:tunisia){Tweet.count(:twitter_id => nil)}
+DataMapper.repository(:iran){User.count}
+DataMapper.repository(:egypt){User.count}
 def old_shit
   DataMapper.repository(:egypt) do 
     Tweet.all.each do |tweet|
@@ -73,43 +77,45 @@ def tunisia_clean
     disallowed_user_keys = ["friends_count", "followers_count"]
     disallowed_tweet_keys = ["id_str"]
     tweet_ids = DataMapper.repository(:tunisia).adapter.select("SELECT id FROM tweets where source is NULL")
-    tweet_ids.each do |tweet_id|
-      tweet = Tweet.first(:id => tweet_id)
-      if !tweet.source
-        puts "Processing tweet from #{tweet.author}"
-        tweet.twitter_id = tweet.link.scan(/statuses\%2F(.*)/).compact.flatten.first.to_i
-        tweet.screen_name = tweet.author
-        tweet.created_at = tweet.pubdate
-        tweet_data,user_data = Utils.tweet_data(tweet.twitter_id) rescue nil
-        debugger
-        if tweet_data && user_data
-          user = User.first({:twitter_id => user_data["id"]}) || User.new
-          tweet_data.keys.each do |key|
-            if tweet.methods.include?(key)
+
+  end
+end
+
+def clean
+  tweet_ids.each do |tweet_id|
+    tweet = Tweet.first(:id => tweet_id)
+    if !tweet.source
+      puts "Processing tweet from #{tweet.author}"
+      tweet.twitter_id = tweet.link.scan(/statuses\%2F(.*)/).compact.flatten.first.to_i
+      tweet.screen_name = tweet.author
+      tweet.created_at = tweet.pubdate
+      tweet_data,user_data = Utils.tweet_data(tweet.twitter_id) rescue nil
+      if tweet_data && user_data
+        user = User.first({:twitter_id => user_data["id"]}) || User.new
+        tweet_data.keys.each do |key|
+          if tweet.methods.include?(key)
+            if key=="id"
+              tweet.send("twitter_id=", tweet_data[key])
+            else
+              tweet.send("#{key}=", tweet_data[key]) if !disallowed_tweet_keys.include?(key)
+            end
+          end
+        end
+        tweet.save
+        if user.new?
+          user.screen_name = tweet.author
+          puts "Saving user #{user.screen_name||user.username}"
+          user_data.keys.each do |key|
+            if user.methods.include?(key)
               if key=="id"
-                tweet.send("twitter_id=", tweet_data[key])
+                user.send("twitter_id=", user_data[key])
               else
-                tweet.send("#{key}=", tweet_data[key]) if !disallowed_tweet_keys.include?(key)
+                user.send("#{key}=", user_data[key]) if !disallowed_user_keys.include?(key)
               end
             end
           end
-          tweet.save
-          if user.new?
-            user.screen_name = tweet.author
-            puts "Saving user #{user.screen_name||user.username}"
-            user_data.keys.each do |key|
-              if user.methods.include?(key)
-                if key=="id"
-                  user.send("twitter_id=", user_data[key])
-                else
-                  user.send("#{key}=", user_data[key]) if !disallowed_user_keys.include?(key)
-                end
-              end
-            end
-            debugger
-            user.save
-            puts "Saved user #{user.screen_name}"
-          end
+          user.save
+          puts "Saved user #{user.screen_name}"
         end
       end
     end
@@ -129,7 +135,6 @@ def egypt_clean
         tweet.screen_name = tweet.author
         tweet.created_at = tweet.pubdate
         tweet_data,user_data = Utils.tweet_data(tweet.twitter_id) rescue nil
-        debugger
         if tweet_data && user_data
           user = User.first({:twitter_id => user_data["id"]}) || User.new
           tweet_data.keys.each do |key|
@@ -154,7 +159,6 @@ def egypt_clean
                 end
               end
             end
-            debugger
             user.save
             puts "Saved user #{user.screen_name}"
           end
