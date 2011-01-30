afe860b5e9f07b8f56dabd5ec9eec243874775f8
peeinears <peeinears@gmail.com>
2011-01-30 13:09:30 -0800

    threads fuck yeah

--- follower_network.rb
+++ follower_network.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'dm-core'
 # require 'dm-migrations'
 # require 'dm-aggregates'
+require 'extensions/array'
 require 'models/graph'
 require 'models/edge'
 require 'models/tweet'
@@ -12,59 +13,37 @@ DataMapper.finalize
 require 'db'
 require 'utils'
 
-MAX_BATCH_SIZE = 500
-THREAD_COUNT = 10
-
 module FollowerNetwork
   
-  def thread_it_out_bitch
-    pages = 
-
+  MAX_BATCH_SIZE = 500
+  THREAD_COUNT = 20
+  
+  def self.create(title='My Great Graph')
     threads = []
-
-    for page in pages
-      threads << Thread.new(page) { |myPage|
-
-        h = Net::HTTP.new(myPage, 80)
-        puts "Fetching: #{myPage}"
-        resp, data = h.get('/', nil )
-        puts "Got #{myPage}:  #{resp.message}"
+    g = Graph.create(:title => title, :style => 'follow')
+    threads = []
+    index = self.user_id_screenname_index
+    user_ids = index.keys#.sort
+    for chunk in user_ids.chunk(THREAD_COUNT)
+      threads << Thread.new(chunk) { |chunk|
+        edges = []
+        puts "Got a chunk of #{chunk.length}."
+        for user_id in chunk
+          followers = Utils.get_followers_from_id(user_id) & user_ids
+          for follower in followers
+            edges << Edge.new({:graph_id => g.id, :start_node => index[follower], :end_node => index[user_id], :style => 'follow'})
+            if edges.length >= MAX_BATCH_SIZE
+              self.bulk_insert_edges(edges)
+              edges.clear
+            end
+          end
+        end
       }
     end
-
-    threads.each { |aThread|  aThread.join }
-    
-    
-    g = Graph.create(:title => title, :style => 'follow')
-    # edges = []
-    threads = []
-    index = user_id_screenname_index
-    user_ids = index.keys#.sort
-    for chunk in user_ids.chunk
-      threads << Thread.new(chunk) { |chunk|
-    end
-    
+    threads.each { |t| t.join }
   end
 
-  def create(title='My Great Graph')
-    g = Graph.create(:title => title, :style => 'follow')
-    edges = []
-    index = user_id_screenname_index
-    user_ids = index.keys#.sort
-    for user_id in user_ids
-      followers = Utils.get_followers_from_id(user_id) & user_ids
-      for follower in followers
-        edges << Edge.new({:graph_id => g.id, :start_node => index[follower], :end_node => index[user_id], :style => 'follow'})
-        if edges.length >= MAX_BATCH_SIZE
-          bulk_insert_edges(edges)
-          edges.clear
-        end
-      end
-    end
-    bulk_insert_edges(edges)
-  end
-
-  def user_id_screenname_index
+  def self.user_id_screenname_index
     index = {}
     users = DataMapper.repository(:default).adapter.select("SELECT twitter_id, screen_name FROM users")
     for user in users
@@ -73,7 +52,7 @@ module FollowerNetwork
     return index
   end
 
-  def bulk_insert_edges(edges)
+  def self.bulk_insert_edges(edges)
     return if edges.empty?
     puts "Bulk inserting #{edges.length} edges."
     sql = "INSERT INTO edges (graph_id, start_node, end_node, style) VALUES"
