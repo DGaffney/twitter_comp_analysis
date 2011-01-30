99e01b090b12738b7c3679f09011af3ee1b752a6
Devin Gaffney <itsme@devingaffney.com>
2011-01-30 15:25:04 -0800

    blah butts

--- new_de_gilader.rb
+++ new_de_gilader.rb
@@ -41,7 +41,7 @@ class NewDeGilader
       tweet_ids = DataMapper.repository(database).adapter.select("SELECT id FROM tweets where source is NULL")
       tweet_id_groupings =  tweet_ids.chunk(HAT_WOBBLE)
       tweet_id_groupings.each do |grouping|
-        Thread.new{|x|run_tweets(database,grouping)}
+        Thread.new{run_tweets(database,grouping)}
       end
     end
   end
