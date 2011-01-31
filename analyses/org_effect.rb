e51deaabbe80a99a0930896824b6ff94d6ed41ba
Devin Gaffney <itsme@devingaffney.com>
2011-01-30 18:01:27 -0800

    finish fof org effects

--- analyses/org_effect.rb
+++ analyses/org_effect.rb
@@ -1,14 +1,14 @@
 #Measure Organizations effect on data
 
 class Analysis::OrgEffect < Analysis
-   def initialize
-      DataMapper.finalize
-      DataMapper.setup(:default, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_1')
-      DataMapper.setup(:tunisia, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_1')
-      DataMapper.setup(:egypt, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_2')
-   end
+  def self.initialize
+    DataMapper.finalize
+    DataMapper.setup(:default, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_1')
+    DataMapper.setup(:tunisia, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_1')
+    DataMapper.setup(:egypt, 'mysql://gonkclub:cakebread@deebee.yourdefaulthomepage.com/140kit_scratch_2')
+  end
 
-  def set_granularity(timechunk)
+  def self.set_granularity(timechunk)
     granularity = case timechunk
       when 'day' then "%Y-%m-%e"
       when 'hour' then "%Y-%m-%e %H"
@@ -16,17 +16,36 @@ class Analysis::OrgEffect < Analysis
     end
   end
 
-  def generate(database, granularity)
+  def self.generate(database, granularity)
    data = DataMapper.repository(database).adapter.select("select date_format(pubdate,'#{granularity}') as date, count(*) as count from tweets group by date_format(pubdate, '#{granularity}');");
-   hashify(data)
+   self.hashify(data)
   end
-
+  
+  def self.generate_for_users(database, granularity, screen_names)
+    data = DataMapper.repository(database).adapter.select("select date_format(pubdate,'#{granularity}') as date, count(*) as count from tweets group by date_format(pubdate, '#{granularity}') where author = \"#{screen_names.join("\" or author = \"")}\";");
+    self.hashify(data)
+  end
+  
+  def self.generate_results
+    users = ["cnnbrk","nytimes","mashable","BreakingNews","TheOnion","TIME","BBCBreaking","TechCrunch","andersoncooper","GStephanopoulos","Newsweek","peoplemag","WSJ","HuffingtonPost","eonline","davidgregory","wired","TheEconomist","NickKristof","GMA","leolaporte","TweetSmarter","sanjayguptaCNN","NBA","Nightline"]
+    Analysis::OrgEffect.initialize
+    Analysis.to_csv(Analysis::OrgEffect.generate(:tunisia, Analysis::OrgEffect.set_granularity("day")), "tunisia_day_full")
+    Analysis.to_csv(Analysis::OrgEffect.generate(:tunisia, Analysis::OrgEffect.set_granularity("hour")), "tunisia_hour_full")
+    Analysis.to_csv(Analysis::OrgEffect.generate(:tunisia, Analysis::OrgEffect.set_granularity("minute")), "tunisia_minute_full")
+    Analysis.to_csv(Analysis::OrgEffect.generate_for_users(:tunisia, Analysis::OrgEffect.set_granularity("day"), users), "tunisia_day_user")
+    Analysis.to_csv(Analysis::OrgEffect.generate_for_users(:tunisia, Analysis::OrgEffect.set_granularity("hour"), users), "tunisia_hour_user")
+    Analysis.to_csv(Analysis::OrgEffect.generate_for_users(:tunisia, Analysis::OrgEffect.set_granularity("minute"), users), "tunisia_minute_user")
+    Analysis.to_csv(Analysis::OrgEffect.generate(:egypt, Analysis::OrgEffect.set_granularity("day")), "egypt_day_full")
+    Analysis.to_csv(Analysis::OrgEffect.generate(:egypt, Analysis::OrgEffect.set_granularity("hour")), "egypt_hour_full")
+    Analysis.to_csv(Analysis::OrgEffect.generate(:egypt, Analysis::OrgEffect.set_granularity("minute")), "egypt_minute_full")
+    Analysis.to_csv(Analysis::OrgEffect.generate_for_users(:egypt, Analysis::OrgEffect.set_granularity("day"), users), "egypt_day_user")
+    Analysis.to_csv(Analysis::OrgEffect.generate_for_users(:egypt, Analysis::OrgEffect.set_granularity("hour"), users), "egypt_hour_user")
+    Analysis.to_csv(Analysis::OrgEffect.generate_for_users(:egypt, Analysis::OrgEffect.set_granularity("minute"), users), "egypt_minute_user")
+  end
   private
 
-  def hashify(data)
+  def self.hashify(data)
     data.map{|d| {"key" => d.date, "value" => d.count }} 
   end
 end
-
-
 
--- analysis.rb
+++ analysis.rb
@@ -1,7 +1,7 @@
 require 'fastercsv'
 
 class Analysis
-  def to_csv(dataset, filename, path="analytical_results/")
+  def self.to_csv(dataset, filename, path="analytical_results/")
     keys, values = dataset.first.keys, dataset.first.values
     FasterCSV.open(path+filename, "w") do |csv|
       csv << keys
@@ -12,5 +12,3 @@ class Analysis
     end
   end
 end
-
-users = ["cnnbrk","nytimes","mashable","BreakingNews","TheOnion","TIME","BBCBreaking","TechCrunch","andersoncooper","GStephanopoulos","Newsweek","peoplemag","WSJ","HuffingtonPost","eonline","davidgregory","wired","TheEconomist","NickKristof","GMA","leolaporte","TweetSmarter","sanjayguptaCNN","NBA","Nightline"]
