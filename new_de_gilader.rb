ac1549c934ce98436bd442a52c014b4134c8ea36
Devin Gaffney <itsme@devingaffney.com>
2011-02-02 22:51:50 -0800

    blergh

--- new_de_gilader.rb
+++ new_de_gilader.rb
@@ -151,8 +151,16 @@ if ARGV.empty?
     NewDeGilader.setup('gonkclub', 'cakebread', 'deebee.yourdefaulthomepage.com', db)
   end
 else
-  db = all_my_bases[ARGV[0]]
-  1.upto(10000) do |x|
-    NewDeGilader.setup('gonkclub', 'cakebread', 'deebee.yourdefaulthomepage.com', db)
+  db,func = all_my_bases[ARGV[0]], ARGV[1]
+  if func
+    puts "User Cleaner"
+    1.upto(10000) do |x|
+      NewDeGilader.setup('gonkclub', 'cakebread', 'deebee.yourdefaulthomepage.com', db)
+    end    
+  else
+    puts "Tweet Cleaner"
+    1.upto(10000) do |x|
+      NewDeGilader.setup('gonkclub', 'cakebread', 'deebee.yourdefaulthomepage.com', db)
+    end
   end
 end
