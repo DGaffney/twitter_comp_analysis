9147a75d3231eee1fcfdecea167a09a4457fe5ec
Devin Gaffney <itsme@devingaffney.com>
2011-02-04 21:32:22 -0800

    update

---
+++ cleanUpWords.php
@@ -0,0 +1,72 @@
+<?php
+
+    $mysql = mysql_connect("deebee.yourdefaulthomepage.com","gonkclub","cakebread") or die(mysql_error());
+	$story = $_GET['story'];
+	
+	if ($story==1){
+		// egypt
+		echo "egypt<br/><br/>";
+		
+		mysql_select_db("---db---",$mysql) or die(mysql_error());
+		$table="----egypt-table----";
+		$maxID=185000;
+	} else {
+		// sidibouzid
+		echo "tunisia<br/><br/>";
+		
+		mysql_select_db("---db---",$mysql) or die(mysql_error());
+		$table="----tunisia-table----";
+		$maxID=167000;
+	}
+	
+	// get all entries
+	$query = "select * from `$story` order by pubdate asc";	
+	$result = mysql_query($query,$mysql) or die(mysql_error());
+	
+	while ($curTweet = mysql_fetch_array($result)) {
+		
+		// get current id row from database
+		$id = $curTweet[id];
+		$cT = strtolower(loseDots($curTweet[text]));
+		$curWords = explode(" ",$cT);	// array of words that make up current tweet
+		$words = '';
+		
+		if (sizeof($curWords) > 0) {
+			
+			foreach($curWords as $word){
+				$words.=$word.',';
+			}
+		}
+	
+		echo $words."</br>";
+	
+		// insert into database
+		if ($words!=''){
+			
+			$query1 = "UPDATE `$story` SET words='{$words}' WHERE id='{$id}'";
+			//echo $query1."</br>";
+			$result1 = mysql_query($query1,$mysql) or die(mysql_error()); 
+			
+		}
+	}
+
+	function loseDots($str){
+		
+		$str = str_replace(",","",$str);
+		$str = str_replace(".","",$str);
+		$str = str_replace("?","",$str);
+		$str = str_replace("!","",$str);
+		$str = str_replace("’","",$str);
+		$str = str_replace(":","",$str);
+		$str = str_replace("'","",$str);
+	//	$str = str_replace("/"," ",$str);
+		$str = str_replace("\"","",$str);
+	//	$str = str_replace("-"," ",$str);
+		$str = str_replace("(","",$str);
+		$str = str_replace(")","",$str);
+		
+		return $str;
+	}
+
+
+?>
